51406f8b00bc45dad96c9e607455184a49040da1
Tom Savage <tsavage@gmail.com>
2020-06-08 00:42:04 -0400

    add department

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ function startPrompt() {
         name: "tracker", 
         type: "list",
         message: "What would you like to do?",
-        choices: ["Add Department", "Add Role", "Add Employee", "View Departments", "View Roles","View Employees", "Update Employee Role", "Exit"]
+        choices: ["Add Department", "Add Role", "Add Employee", "View Departments", "View Roles", "View Employees", "Update Employee Role", "Exit"]
     }).then(function (answer){
         console.log("You Selected: "+ answer.tracker);
 
@@ -31,24 +31,24 @@ function startPrompt() {
             case "Add Department":
                 addDepartment();
                 break;
-            case "Add Role":
-                addRole();
-                break;
-            case "Add Employee":
-                addEmployee();
-                break;
-            case "View Departments":
-                viewDepartments();
-                break;
-            case "View Roles":
-                viewRoles();
-                break;
-            case "View Employees":
-                viewEmployees();
-                break;
-            case "Update Employee Role":
-                updateRole();
-                break;
+            // case "Add Role":
+            //     addRole();
+            //     break;
+            // case "Add Employee":
+            //     addEmployee();
+            //     break;
+            // case "View Departments":
+            //     viewDepartments();
+            //     break;
+            // case "View Roles":
+            //     viewRoles();
+            //     break;
+            // case "View Employees":
+            //     viewEmployees();
+            //     break;
+            // case "Update Employee Role":
+            //     updateEmployeeRole();
+            //     break;
             default:
                 exit();
         }
@@ -57,5 +57,23 @@ function startPrompt() {
     );
 }
 
+function addDepartment(){
+    inquirer.prompt({
+        type: "input",
+        message: "Enter the name of the department to be added:",
+        name: "departmentName"
+    }).then (function(answer) {
+        connection.query("INSERT INTO department (name) VALUES (?)", [answer.departmentName], function(err, res) {
+            if (err) throw err;
+            console.table(res)
+            console.log("Successfully added Department!");
+            startPrompt(); 
+    })
+});
+}
 
+function exit(){
+    connection.end();
+    process.exit();
+}
 
